87c883135fce9825f25390f7aa9c419df3936310
[Aman Yadav] <[aman4864y@gmail.com]>
2025-03-14 17:09:23 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/GuessTheNumber.css 	new file:   src/GuessTheNumber.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GuessTheNumber from './GuessTheNumber';
+
+function App() {
+  return (
+    <div className="App">
+      <GuessTheNumber />
+    </div>
+  );
+}
+
+export default App;
